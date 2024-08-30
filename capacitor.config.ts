761d3ba2f23f4e6bc4ae758b48c4d2a9ebc8669c
Francisco Belda <francisco.belda@cieep.com>
2024-08-30 16:47:20 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'componentes-ionic-v8',
+  webDir: 'www'
+};
+
+export default config;
